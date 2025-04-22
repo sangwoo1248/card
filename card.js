62b6c334f267babe392e37b26ef97f7b2904ba81
oct <sangwoo1248@gmail.com>
2025-04-23 02:05:45 +0900

    ver2

--- card.js
+++ card.js
@@ -1,29 +1,4 @@
-/*function allowDrop(event) {
-  event.preventDefault();  // 드롭
-}
-
-document.querySelectorAll('.image-grid img').forEach(img => {
-  img.addEventListener("dragstart", event => {
-      event.dataTransfer.setData("text/plain", event.target.id);
-  });
-});
-
-function drop(event) {
-  event.preventDefault();
-
-  const imageId = event.dataTransfer.getData("text/plain");
-  const draggedImage = document.getElementById(imageId);
-
-  if (draggedImage) {
-      event.target.appendChild(draggedImage);
-
-      draggedImage.style.width = "80px";
-      draggedImage.style.height = "auto";
-      draggedImage.style.margin = "5px";
-  }
-}*/
-
-// 드롭
+/*
 function allowDrop(event) {
   event.preventDefault();
 }
@@ -59,4 +34,44 @@ function drop(event) {
     }
   }
 }
+*/
 
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+// card 이동
+function drag(event) {
+  event.dataTransfer.setData("text/plain", event.target.parentElement.id);  
+}
+
+// 슬롯에 저장
+function drop(event) {
+  event.preventDefault();
+
+  const containerId = event.dataTransfer.getData("text/plain");
+  const cardContainer = document.getElementById(containerId);
+
+  if (cardContainer && !event.target.querySelector('.card-container')) {
+      event.target.appendChild(cardContainer);
+  }
+}
+
+// X버튼 클릭 시 원래 자리로 이동
+function returnToOriginal(containerId) {
+  const cardContainer = document.getElementById(containerId);
+  const originalArea = document.getElementById("return");
+
+  if (cardContainer) {
+      originalArea.appendChild(cardContainer);
+  }
+}
+
+/*function returnToOriginal(containerId) {
+  const cardContainer = document.getElementById(containerId);
+  const originalSlot = document.querySelector('.image-grid');
+
+  if (cardContainer) {
+      originalSlot.appendChild(cardContainer);
+  }
+}*/
